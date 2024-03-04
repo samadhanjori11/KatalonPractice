7e54abcb82cdeda51031f67beeeeec445e65ccca
joris <joris@DESKTOP-6JFGPOU>
2024-03-04 22:50:28 +0530

    ecommerce Test suite

---
+++ Test Suites/TS3-LambdatestPlayground.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Ecommerce site</description>
+   <name>TS3-LambdatestPlayground</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>48b88fdc-4250-4714-af46-c6b5c70f2349</testSuiteGuid>
+   <testCaseLink>
+      <guid>e6c0dbbc-766a-4679-a118-58386b67b541</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LambdaTestPlayground/TC01_Access ecommerce website</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>34377cea-4ea4-464a-a2fb-474c7855f82e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LambdaTestPlayground/TC02_Successful Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>95a3c163-7f42-41ab-b113-12f1bbf2e5cd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LambdaTestPlayground/TC03_Invalid Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
